648196ba8a4372b50725dc8d2a134335bfa23244
Tony Li <lihenantony1997@gmail.com>
2024-06-05 21:40:17 -0700

    ddpm

--- ddpm.py
+++ ddpm.py
@@ -190,59 +190,6 @@ class AttentionBlock(nn.Module):
         return res
 
 
-class AttentionBlock2(nn.Module):
-    def __init__(self, n_channels: int, n_heads: int = 1, d_k: int = None, n_groups: int = 32):
-        super().__init__()
-        if d_k is None:
-            d_k = n_channels
-        # Normalization layer
-        self.norm = nn.GroupNorm(n_groups, n_channels)
-        # Projections for query, key and values
-        self.projection = nn.Linear(n_channels, n_heads * d_k * 3)
-        # Linear layer for final transformation
-        self.output = nn.Linear(n_heads * d_k, n_channels)
-        # Scale for dot-product attention
-        self.scale = d_k ** -0.5
-        #
-        self.n_heads = n_heads
-        self.d_k = d_k
-
-    def forward(self, x: torch.Tensor, t: Optional[torch.Tensor] = None):
-        """
-        * `x` has shape `[batch_size, in_channels, height, width]`
-        * `t` has shape `[batch_size, time_channels]`
-        """
-        # `t` is not used, but it's kept in the arguments because for the attention layer function signature
-        # to match with `ResidualBlock`.
-        _ = t
-        # Get shape
-        batch_size, n_channels, height, width = x.shape
-        # Change `x` to shape `[batch_size, seq, n_channels]`
-        x = x.view(batch_size, n_channels, -1).permute(0, 2, 1)
-        # Get query, key, and values (concatenated) and shape it to `[batch_size, seq, n_heads, 3 * d_k]`
-        qkv = self.projection(x).view(
-            batch_size, -1, self.n_heads, 3 * self.d_k)
-        # Split query, key, and values. Each of them will have shape `[batch_size, seq, n_heads, d_k]`
-        q, k, v = torch.chunk(qkv, 3, dim=-1)
-        # Calculate scaled dot-product $\frac{Q K^\top}{\sqrt{d_k}}$
-        attn = torch.einsum('bihd,bjhd->bijh', q, k) * self.scale
-        # Softmax along the sequence dimension $\underset{seq}{softmax}\Bigg(\frac{Q K^\top}{\sqrt{d_k}}\Bigg)$
-        attn = attn.softmax(dim=2)
-        # Multiply by values
-        res = torch.einsum('bijh,bjhd->bihd', attn, v)
-        # Reshape to `[batch_size, seq, n_heads * d_k]`
-        res = res.reshape(batch_size, -1, self.n_heads * self.d_k)
-        # Transform to `[batch_size, seq, n_channels]`
-        res = self.output(res)
-
-        # Add skip connection
-        res += x
-
-        # Change to shape `[batch_size, in_channels, height, width]`
-        res = res.permute(0, 2, 1).view(batch_size, n_channels, height, width)
-
-        #
-        return res
     
 
 class DownBlock(nn.Module):
